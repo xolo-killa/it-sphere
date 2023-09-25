f173a1121d0af505e4706afdc6d0930f86d0defa
xolo-killa <134775308+xolo-killa@users.noreply.github.com>
2023-09-25 17:51:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Скрипт для переключения между вкладками и отображения информации
 const tabButtons = document.querySelectorAll(".tab-button");
 const tabContents = document.querySelectorAll(".tab-content .job-info");
 
@@ -6,19 +5,16 @@ tabButtons.forEach((button) => {
     button.addEventListener("click", () => {
         const targetId = button.getAttribute("data-target");
 
-        // Переключаем класс .active для вкладок
         tabButtons.forEach((btn) => {
             btn.classList.remove("active");
         });
         button.classList.add("active");
 
-        // Скрыть все секции с информацией о профессиях
         tabContents.forEach((content) => {
             content.style.opacity = 0;
             content.style.maxHeight = "0";
         });
 
-        // Показать только выбранную секцию
         const targetContent = document.getElementById(targetId);
         if (targetContent) {
             targetContent.style.opacity = 1;
@@ -27,7 +23,6 @@ tabButtons.forEach((button) => {
     });
 });
 
-// Изначально показать первую вкладку при загрузке страницы
 if (tabContents.length > 0) {
     tabContents[0].style.opacity = 1;
     tabContents[0].style.maxHeight = "1000px";
